09ec6a52516ccb06b4cb23d69311f399d33140d4
Arka <aasa.aho@gmail.com>
2026-09-15 08:08:34 +0300

    Added more tests to LinkedListTests

--- Ships.Tests/LinkedListTests.cs
+++ Ships.Tests/LinkedListTests.cs
@@ -13,4 +13,66 @@ public class LinkedListTests
 
 		Assert.Equal(3, list.Count);
 	}
+	
+	[Fact]
+	public void TestNewListIsEmpty()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+ 
+		Assert.Equal(0, list.Count);
+		Assert.Empty(list);
+	}
+	
+	
+	[Fact]
+	public void TestReturnsCorrectSavedValue()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+		list.Add(77);
+		list.Add(66);
+		list.Add(55);
+
+		Assert.Equal(55, list.ElementAt(2));
+	}
+	
+	[Fact]
+	public void TestRemoveReturnsTrueAndDecreasesCount()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+		list.Add(6);
+		list.Add(7);
+		list.Add(8);
+ 
+		bool removed = list.Remove(6);
+ 
+		Assert.True(removed);
+		Assert.Equal(2, list.Count);
+	}
+	
+	[Fact]
+	public void TestRemoveNonExistingReturnsFalseAndDoesNotDecreasesCount()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+		list.Add(6);
+		list.Add(7);
+ 
+		bool removed = list.Remove(666);
+ 
+		Assert.False(removed);
+		Assert.Equal(2, list.Count);
+	}
+	
+	[Fact]
+	public void TestClearRemovesAll()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+		list.Add(6);
+		list.Add(7);
+ 
+		list.Clear();
+ 
+		Assert.Equal(0, list.Count);
+		Assert.Empty(list);
+		Assert.False(list.Contains(1));
+	}
 }
